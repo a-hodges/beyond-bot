102f0dc59671eda2d325a4a7d2644f8f625af868
b-hodges <16946799+b-hodges@users.noreply.github.com>
2018-10-24 12:59:29 -0500

    Fixed character colors

--- cogs/beyondapi.py
+++ cogs/beyondapi.py
@@ -403,7 +403,7 @@ class Character:
         r = int(color[1:3], 16)
         g = int(color[3:5], 16)
         b = int(color[5:7], 16)
-        return r * 256 + g * 16 + b
+        return r * 256 * 256 + g * 256 + b
 
     def embed_fields(self):
         for c in self.json['classes']:
